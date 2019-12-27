6e9e4e95fb07fa6abd46e6cd0baae63c31c35642
Ahn Junho <ahn9807@gmail.com>
2019-12-27 23:30:40 +0900

    12/27 기초 공사 완료!

---
+++ app/src/main/java/com/example/bitgaram/main/bitgaram/presenter/main/fragment/AddressData.java
@@ -0,0 +1,11 @@
+package com.example.bitgaram.main.bitgaram.presenter.main.fragment;
+
+public class AddressData {
+    public String name;
+    public String phone;
+
+    public AddressData(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+}
--- app/src/main/java/com/example/bitgaram/main/bitgaram/presenter/main/fragment/AddressFragment.java
+++ app/src/main/java/com/example/bitgaram/main/bitgaram/presenter/main/fragment/AddressFragment.java
@@ -1,19 +1,23 @@
 package com.example.bitgaram.main.bitgaram.presenter.main.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.bitgaram.R;
 
+import java.util.ArrayList;
+
 public class AddressFragment extends Fragment {
+    private ArrayList<AddressData> addresses;
 
     public static AddressFragment newInstance(){
         AddressFragment addressFragment = new AddressFragment();
@@ -28,7 +32,15 @@ public class AddressFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.addressfragment, container, false);
-        return view;
-    }
+        addresses = new ArrayList<AddressData>();
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.addressfragment, container, false);
+        com.example.bitgaram.main.bitgaram.presenter.main.fragment.AddressManager.getContacts(this.getContext(), addresses);
+
+        AddressAdapter addressAdapter = new AddressAdapter(addresses);
+        RecyclerView recyclerView = rootView.findViewById(R.id.addressView);
+        recyclerView.setAdapter(addressAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+
+        return recyclerView;
+}
 }
