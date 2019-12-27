0dba23d42e67abf47b0dbfd4e51e8b9574cc56fc
forbid403 <forbid403@naver.com>
2019-12-27 22:24:22 +0900

    [complete] gallery 기본 기능 구현 완료

---
+++ app/src/main/java/com/example/bitgaram/main/bitgaram/presenter/main/fragment/GalleryGridAdapter.java
@@ -0,0 +1,87 @@
+package com.example.bitgaram.main.bitgaram.presenter.main.fragment;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.PopupWindow;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.content.ContextCompat;
+
+import com.example.bitgaram.R;
+
+public class GalleryGridAdapter extends BaseAdapter{
+    LayoutInflater inflater;
+    int layout;
+    int images[];
+    Context context;
+
+    public GalleryGridAdapter(Context context, int layout, int[] img){
+        this.layout = layout;
+        this.images = img;
+        this.context = context;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return images[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, final ViewGroup parent) {
+        if(convertView == null) convertView = inflater.inflate(layout, null);
+        final ImageView imageView = (ImageView)convertView.findViewById(R.id.imageCell);
+        imageView.setImageResource(images[position]);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+            @Override
+            public void onClick(View v) {
+
+                LayoutInflater layoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View popup = layoutInflater.inflate(R.layout.imagedetail, null);
+                popup.setBackground(ContextCompat.getDrawable(context, images[position]));
+                final PopupWindow popupWindow = new PopupWindow(popup, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+                popupWindow.showAtLocation(v, Gravity.CENTER, 0, 0);
+                popupWindow.setFocusable(true);
+                v.setAlpha(0.5F);
+
+                popup.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        popupWindow.dismiss();
+                        return true;
+                    }
+                });
+
+
+            }
+        });
+
+        return convertView;
+    }
+
+
+}
