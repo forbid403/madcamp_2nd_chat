49a254c38202d4276241ab54a4f6f7711a365123
forbid403 <forbid403@naver.com>
2020-01-04 15:19:43 +0900

    [feat] add one address

--- routes/address.js
+++ routes/address.js
@@ -1,18 +1,21 @@
 const express = require('express')
 const address = express.Router()
+const User = require('../models/userInfo')
 
 //add one address
-address.get('/address/:address', (req, res)=>{
-    console.log("/address/"+req.params.address)
-    
-    //find user
-    /*
-        do something
-    */
+address.get('/:phone/:address', (req, res)=>{
+    console.log("/address/"+req.params.phone + "/" +req.params.address)
 
-    //save address
-    
-
+    User.updateOne({"phone" : req.params.phone}, {$addToSet : {'address' : req.params.address}}).exec((err, result)=>{
+        if(err) return res.status(500).json({"err" : err})
+        if(!result.n) return res.status(500).json({"err" : "user not found!"})
+        if(!result.nModified) return res.status(500).json({"err" : "address already in!"})
+                
+        res.json({res : 1})
+    })
 })
+
+
+
 
 module.exports = address
